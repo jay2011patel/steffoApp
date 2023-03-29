f0477e6490edd35ff043c11530932c7925541391
Dhyey <dhyeyshah28801@gmail.com>
2023-03-29 17:42:36 +0530

    User approval along with parent assignment

--- lib/pages/UserRequestPage.dart
+++ lib/pages/UserRequestPage.dart
@@ -9,10 +9,11 @@ import 'HomePage.dart';
 
 class UserRequestPage extends StatelessWidget {
   final User user;
-  const UserRequestPage({Key? key, required this.user}) : super(key: key);
+  UserRequestPage({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    loadDistributors();
     return Scaffold(
       appBar: appbar("Requests", () {
         print("Back Pressed");
@@ -134,22 +135,40 @@ class UserRequestPage extends StatelessWidget {
             margin: EdgeInsets.only(top: 5),
             padding: EdgeInsets.symmetric(horizontal: 10),
             child: ElevatedButton(
+                child: Text("Approve"),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color.fromRGBO(102, 178, 83, 1.0)
                 ),
-                onPressed: () async{
-                  var f =0;
-                  await AlertDialog();
-
-                  if(f ==1){
+                onPressed: () async {
+                  var f = 0;
+                  if(user.userType == "Dealer"){
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            child: DistributorDialog(context),
+                          );
+                          // final res = await http.post(
+                          //   Uri.parse(
+                          //       "http://urbanwebmobile.in/steffo/approveuser.php"),
+                          //   body: {"decision": "Approve", "id": user.id},
+                          // );
+                        });
+                  }else{
                     final res = await http.post(
                       Uri.parse(
-                          "http://urbanwebmobile.in/steffo/approveuser.php"),
-                      body: {"decision": "Approve", "id": user.id},
+                          "http://urbanwebmobile.in/steffo/setdealerparent.php"),
+                      body: {
+                        "id": user.id,
+                        "parentId": "0"
+                      },
                     );
+                    Navigator.popUntil(context, ModalRoute.withName('/home'));
+
+
                   }
-                },
-                child: Text("Approve")
+                }
+
             ),
           ),
           Container(
@@ -185,5 +204,121 @@ class UserRequestPage extends StatelessWidget {
         ],
       ),
     );
+
+
   }
+  var f = 0;
+  List<User> distributors = [];
+  loadDistributors() async {
+    if(f==0){
+      final res = await http.post(
+          Uri.parse("http://urbanwebmobile.in/steffo/getdistributors.php")
+      );
+      var responseData = jsonDecode(res.body);
+      for (int i = 0; i < responseData["data"].length; i++) {
+        User u = User();
+        u.id = responseData['data'][i]['id'];
+        u.firstName = responseData['data'][i]['firstName'];
+        u.lastName = responseData['data'][i]['lastName'];
+        u.email = responseData['data'][i]['email'];
+        u.mobileNumber = responseData['data'][i]['mobileNumber'];
+        u.parentId = responseData['data'][i]['parentId'];
+        u.userType = responseData['data'][i]['userType'];
+        u.userStatus = responseData['data'][i]['userStatus'];
+        u.orgName = responseData['data'][i]['orgName'];
+        u.gstNumber = responseData['data'][i]['gstNumber'];
+        u.panNumber = responseData['data'][i]['panNumber'];
+        u.adhNumber = responseData['data'][i]['adhNumber'];
+        u.address = responseData['data'][i]['address'];
+        distributors.add(u);
+      }
+    f = 1;
+    }
+  }
+  Widget DistributorDialog(context)  {
+
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height/2,
+        child: Column(
+          children: [
+            Container(
+              child: Text("Dealer Address",style: TextStyle(fontFamily: "Poppins_Bold"),),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(user.address!,style: TextStyle(fontFamily: "Poppins"),),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text("Select Distributor",style: TextStyle(fontFamily: "Poppins_Bold"),),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height/3,
+              child: SingleChildScrollView(
+                physics: ScrollPhysics(),
+                child:  Container(
+                  child: ListView.builder(
+                      itemCount: distributors.length,
+                      physics: NeverScrollableScrollPhysics(),
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                            onTap: () async {
+                              var f =0 ;{
+                              if(f==0){
+                                final res = await http.post(
+                                Uri.parse(
+                                    "http://urbanwebmobile.in/steffo/setdealerparent.php"),
+                                  body: {
+                                    "id": user.id,
+                                    "parentId": distributors[index].id
+                                  },
+                                );
+                                }
+                                f=1;
+                              }
+
+                              if(f==1){
+
+                                Navigator.popUntil(context, ModalRoute.withName('/home'));
+
+                              }
+
+                            },
+                            child: Card(
+                              margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                              elevation: 4,
+                              child: Column(
+                                children: [
+                                  Container(
+                                      child: Text(distributors[index].orgName!,textAlign: TextAlign.left,style: TextStyle(fontSize: 15),),
+                                    margin: EdgeInsets.all(10),
+                                    padding: EdgeInsets.all(5) ,
+                                    width: MediaQuery.of(context).size.width,
+                                    color: Colors.blueAccent,
+                                  ),
+                                  Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 20),
+                                      margin: EdgeInsets.only(bottom: 10),
+                                      child: Text(distributors[index].address!,style: TextStyle(fontSize: 15))
+                                  ),
+                                ],
+                              ),
+                            )
+                        );
+                      },
+                    ),
+                ),
+              ),
+            )
+
+
+
+          ],
+        ),
+    );
+  }
+
 }
