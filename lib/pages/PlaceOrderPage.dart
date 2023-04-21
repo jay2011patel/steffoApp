2abedda8a08d885515a6a2c6eab5576c7337bbb7
Dhyey <dhyeyshah28801@gmail.com>
2023-04-21 15:00:44 +0530

    Order From Lumpsum Added

--- lib/pages/PlaceOrderPage.dart
+++ lib/pages/PlaceOrderPage.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stefomobileapp/ui/cards.dart';
 import '../Models/grade.dart';
+import '../Models/lumpsum.dart';
 import '../Models/region.dart';
 import '../Models/size.dart';
 import '../ui/common.dart';
@@ -92,10 +95,43 @@ class _PlaceOrderPageState extends State<PlaceOrderContent> {
   TextEditingController party_mob_num = TextEditingController();
   TextEditingController loading_type = TextEditingController();
   TextEditingController base_price = TextEditingController();
+  List<Lumpsum> lumpsumList = [];
+  bool isInventoryDataLoaded= false;
+  loadLumpsumData() async {
+    print("inLoadLumpsumData");
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final user_id = await prefs.getString('id');
+
+    var res = await http.post(
+        Uri.parse("http://urbanwebmobile.in/steffo/getinventory.php"),
+        body: {
+          "user_id": user_id,
+        });
+
+    var responseData = jsonDecode(res.body);
+    var orders = [];
+
+    print(responseData);
+    for (int i = 0; i < responseData["data"].length; i++) {
+      print(responseData['data'][i]['name']);
+      Lumpsum l = Lumpsum();
+      l.orderId = responseData["data"][i]["order_id"];
+      l.name = responseData["data"][i]["name"];
+      l.qty = responseData["data"][i]["qty_left"];
+      l.price = responseData["data"][i]["price"];
+      l.status = responseData["data"][i]["status"];
+      l.id = responseData['data'][i]["ls_id"];
+      l.date = responseData["data"][i]["createdAt"];
+      lumpsumList.add(l);
+    }
+    isInventoryDataLoaded = true;
+    setState(() {});
+  }
+
 
   @override
   void initState() {
-
+    loadLumpsumData();
     super.initState();
     focusNode1 = FocusNode();
     focusNode2 = FocusNode();
@@ -225,7 +261,17 @@ class _PlaceOrderPageState extends State<PlaceOrderContent> {
   
   final List<Map<String, String>> listOfColumns = [];
   onPlaceOrder() async {
-    if(selectedOrderType != "Use Lumpsum"){
+    if(selectedOrderType == "Use Lumpsum"){
+      for(int  i = 0 ; i < reductionData.length ; i++){
+        var res = await http.post(
+            Uri.parse("http://urbanwebmobile.in/steffo/updateinventory.php"),
+            body: {
+              "id": reductionData[i]["id"],
+              "qty": reductionData[i]["qty"]
+            }
+        );
+      }
+    }
       var res = await http.post(
         Uri.parse("http://urbanwebmobile.in/steffo/placeorder.php"),
         body: selectedOrderType == "Lump-sum"
@@ -268,7 +314,7 @@ class _PlaceOrderPageState extends State<PlaceOrderContent> {
       var responseData = json.decode(res.body);
       print(responseData["value"].toString());
 
-      if (responseData["status"] == '200' && selectedOrderType == "With Size") {
+      if (responseData["status"] == '200' && selectedOrderType != "Lump-sum") {
         for (int i = 0; i < listOfColumns.length; i++) {
           http.post(
             Uri.parse("http://urbanwebmobile.in/steffo/setorder.php"),
@@ -294,12 +340,11 @@ class _PlaceOrderPageState extends State<PlaceOrderContent> {
         }
       }
       // print(listOfColumns[0]['Name']);
-    }else{
-      print("Select From Lumpsum Type Order Selected");
-    }
+
   }
 
   num tCost = 0;
+  var reductionData =[];
   Widget PlaceOrderBody() {
     loadItemData();
     // List<DropdownMenuItem<String>> dropdownItems = [];
@@ -843,57 +888,155 @@ class _PlaceOrderPageState extends State<PlaceOrderContent> {
                             minimumSize: const Size(190, 40)),
                         onPressed: () {
                           var grdpct,szpct = 0;
-                          if (
-                              // selectedValue != null &&
-                              selectedGrade != null &&
-                                  selectedSize != null &&
-                                  qty.text != "" && base_price.text != "" && selectedRegion != null && selectedTransType != null) {
-
-                            for(int i  = 0 ; i  < gradeList.length;i++){
-                              if( gradeList[i].value == selectedGrade){
-                                grdpct = int.parse(gradeList[i].price!);
-                              }
-                            }
-                            for(int i  = 0 ; i  < sizeList.length;i++){
-                              if( sizeList[i].value == selectedSize){
-                                szpct = int.parse(sizeList[i].price!);
-                              }
-                            }
-                            setState(() {
-
-                              isItem = " ";
-                              int f = 0;
-                              for (int i = 0; i < listOfColumns.length; i++) {
-                                if (listOfColumns.elementAt(i)["Name"]! ==
-                                    // "$selectedValue"
-                                    "$selectedGrade $selectedSize") {
-                                  int quty = int.parse(
-                                      listOfColumns.elementAt(i)["Qty"]!);
-                                  quty = quty + int.parse(qty.text);
-                                  num p = selectedTransType=="CIF" && selectedOrderType != "Lump-sum" ?(int.parse(base_price.text) + grdpct + szpct +tCost) * quty :(int.parse(base_price.text) + grdpct + szpct +0) * quty;
-
-                                  listOfColumns.elementAt(i)["Qty"] =
-                                      quty.toString();
-                                  listOfColumns.elementAt(i)["Price"] =
-                                      p.toString();
-                                  f = 1;
+                          if(selectedOrderType != "Use Lumpsum"){
+                            if (selectedGrade != null &&
+                                selectedSize != null &&
+                                qty.text != "" &&
+                                base_price.text != "" &&
+                                selectedRegion != null &&
+                                selectedTransType != null) {
+                              for (int i = 0; i < gradeList.length; i++) {
+                                if (gradeList[i].value == selectedGrade) {
+                                  grdpct = int.parse(gradeList[i].price!);
                                 }
                               }
-                              if (f == 0) {
-                                listOfColumns.add({
-                                  "Sr_no": itemNum.toString(),
-                                  "Name": "$selectedGrade $selectedSize",
-                                  "Qty": qty.text,
-                                  "Price": selectedTransType=="CIF" && selectedOrderType != "Lump-sum" ?((int.parse(base_price.text) + grdpct + szpct + tCost) * int.parse(qty.text)).toString():((int.parse(base_price.text) + grdpct + szpct + 0) * int.parse(qty.text)).toString()
-                                });
-                                itemNum = itemNum + 1;
+                              for (int i = 0; i < sizeList.length; i++) {
+                                if (sizeList[i].value == selectedSize) {
+                                  szpct = int.parse(sizeList[i].price!);
+                                }
                               }
-                            });
-                            totalQuantity = totalQuantity + int.parse(qty.text);
-                            //print(totalQuantity);
-                          } else {
-                            isItem = "Please Enter All of the above fields";
-                            setState(() {});
+                              setState(() {
+                                isItem = " ";
+                                int f = 0;
+                                for (int i = 0; i < listOfColumns.length; i++) {
+                                  if (listOfColumns.elementAt(i)["Name"]! ==
+                                      // "$selectedValue"
+                                      "$selectedGrade $selectedSize") {
+                                    int quty = int.parse(
+                                        listOfColumns.elementAt(i)["Qty"]!);
+                                    quty = quty + int.parse(qty.text);
+                                    num p = selectedTransType == "CIF" &&
+                                            selectedOrderType != "Lump-sum"
+                                        ? (int.parse(base_price.text) +
+                                                grdpct +
+                                                szpct +
+                                                tCost) *
+                                            quty
+                                        : (int.parse(base_price.text) +
+                                                grdpct +
+                                                szpct +
+                                                0) *
+                                            quty;
+
+                                    listOfColumns.elementAt(i)["Qty"] =
+                                        quty.toString();
+                                    listOfColumns.elementAt(i)["Price"] =
+                                        p.toString();
+                                    f = 1;
+                                  }
+                                }
+                                if (f == 0) {
+                                  listOfColumns.add({
+                                    "Sr_no": itemNum.toString(),
+                                    "Name": "$selectedGrade $selectedSize",
+                                    "Qty": qty.text,
+                                    "Price": selectedTransType == "CIF" &&
+                                            selectedOrderType != "Lump-sum"
+                                        ? ((int.parse(base_price.text) +
+                                                    grdpct +
+                                                    szpct +
+                                                    tCost) *
+                                                int.parse(qty.text))
+                                            .toString()
+                                        : ((int.parse(base_price.text) +
+                                                    grdpct +
+                                                    szpct +
+                                                    0) *
+                                                int.parse(qty.text))
+                                            .toString()
+                                  });
+                                  itemNum = itemNum + 1;
+                                }
+                              });
+                              totalQuantity =
+                                  totalQuantity + int.parse(qty.text);
+                              //print(totalQuantity);
+                            } else {
+                              isItem = "Please Enter All of the above fields";
+                              setState(() {});
+                            }
+                          }else{
+                            showDialog (
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  elevation: 16,
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    //height: 400,
+                                    child: Column(
+                                      children: [
+                                        Container(child: Text("Select The Lumpsum",style: GoogleFonts.poppins(textStyle: TextStyle()))),
+                                        SingleChildScrollView(
+                                          child: Container(
+                                            height: MediaQuery.of(context).size.height/2,
+                                            child: ListView.builder(
+                                                itemCount: lumpsumList.length,
+                                                itemBuilder: (context,index){
+                                                  return LayoutBuilder(
+                                                    builder: (context,constraints) {
+                                                      if(lumpsumList[index].name == "$selectedGrade $selectedSize" && int.parse(lumpsumList[index].qty!) > int.parse(qty.text)){
+                                                        return Container(
+                                                          margin: EdgeInsets.only(top: 10),
+                                                            child: InkWell(
+                                                              onTap: (){
+                                                                listOfColumns.add({
+                                                                  "Sr_no": itemNum.toString(),
+                                                                  "Name": lumpsumList[index].name!,
+                                                                  "Qty": qty.text,
+                                                                  "Price": selectedTransType == "CIF" &&
+                                                                      selectedOrderType != "Lump-sum"
+                                                                      ? ((int.parse(lumpsumList[index].price!)+tCost) *
+                                                                      int.parse(qty.text))
+                                                                      .toString()
+                                                                      : ((int.parse(lumpsumList[index].price!)) *
+                                                                      int.parse(qty.text))
+                                                                      .toString()
+                                                                });
+                                                                lumpsumList[index].qty = (int.parse(lumpsumList[index].qty!) - int.parse(qty.text)).toString();
+
+                                                                reductionData.add({
+                                                                  "id": lumpsumList[index].id,
+                                                                  "qty": lumpsumList[index].qty
+                                                                });
+                                                                itemNum = itemNum + 1;
+                                                                setState(() {
+
+                                                                });
+                                                                Navigator.pop(context);
+                                                              },
+                                                              child: Card(
+                                                                elevation: 15,
+                                                                child: InventoryCard(
+                                                                    context,
+                                                                    lumpsumList[index]),
+                                                              ),
+                                                            ));
+                                                      }else{
+                                                        return Container();
+                                                      }
+                                                    }
+                                                  );
+                                                }),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ),
+                                );
+                              },
+                            );
                           }
                         },
                         child: const Text("Add Item")
